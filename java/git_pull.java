1880da6c25bad17ccd0a4845ba701ac55492b671
cefalopodo-tj <63636399+cefalopodo-tj@users.noreply.github.com>
2020-04-15 09:08:54 +0200

    Se crea el archivo para ver git pull en carpeta java
    Para el curso git es un ejemplo cualquiera para ver GIT PULL

---
+++ java/git_pull.java
@@ -0,0 +1,12 @@
+package vamosaver;
+
+public class Ej_git_pull {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ejemplo del curso git/git hub video 12 para ver el uso de git pull desde interfaz");
+		
+		
+	}
+
+}
